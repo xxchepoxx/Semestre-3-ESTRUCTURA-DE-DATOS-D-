076e1141748fd5950368eea2c51634d535c45d4e
marcelo chacon <shadoweraira123natior@gmail.com>
2026-06-12 17:24:13 -0500

    cambios

--- UNIDAD 1/PRACTICO EXPERIMENTAL 1/Tarea - Práctico Experimental 01/obj/Debug/net10.0/Tarea - Práctico Experimental 01.AssemblyInfo.cs
+++ UNIDAD 1/PRACTICO EXPERIMENTAL 1/Tarea - Práctico Experimental 01/obj/Debug/net10.0/Tarea - Práctico Experimental 01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarea - Práctico Experimental 01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+638bcb60f282ee34dbc913a72daa8cb7de4592ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36b192c11850c8b1e450bdeeb42538844c5e6e12")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarea - Práctico Experimental 01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarea - Práctico Experimental 01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
