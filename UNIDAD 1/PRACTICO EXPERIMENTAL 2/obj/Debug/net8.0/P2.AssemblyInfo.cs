88fde4634a06f9571bcaf044ac41079c29ef6e51
marcelo chacon <shadoweraira123natior@gmail.com>
2026-07-19 10:00:40 -0500

    git ignore

--- UNIDAD 1/PRACTICO EXPERIMENTAL 2/obj/Debug/net8.0/P2.AssemblyInfo.cs
+++ UNIDAD 1/PRACTICO EXPERIMENTAL 2/obj/Debug/net8.0/P2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d72163d3e9bdf109fbb62bbe7be8f362cd029425")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48eb65e8ac4a242469de3283c972918b52b3fb51")]
 [assembly: System.Reflection.AssemblyProductAttribute("P2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
